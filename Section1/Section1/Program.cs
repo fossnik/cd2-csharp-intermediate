b481fe59bd9b6d25a68e0ff72a909bd175e23a59
Zack Hartmann <zackhartmann@gmail.com>
2017-11-28 11:44:16 -0500

    initial commit

---
+++ Section1/Section1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
