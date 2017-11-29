37c4d49b3ea7c20003f6ae7393bdf2e8cf7c7437
Zack Hartmann <zackhartmann@gmail.com>
2017-11-28 19:33:02 -0500

    Se2Le9

---
+++ Se2Le9/Point.cs
@@ -0,0 +1,25 @@
+﻿namespace Se2Le9
+{
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            this.X = newLocation.X;
+            this.Y = newLocation.Y;
+        }
+    }
+}
