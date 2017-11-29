3c56844e6b3446493afb7c9c646251f2c547882a
Zack Hartmann <zackhartmann@gmail.com>
2017-11-29 15:38:03 -0500

    Tryparse method implemented

--- Se2Le9/Program.cs
+++ Se2Le9/Program.cs
@@ -8,7 +8,23 @@ namespace Se2Le9
     {
         public static void Main(string[] args)
         {
-            
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+        }
+
+        public static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
         }
 
         public static void UsePoints()
