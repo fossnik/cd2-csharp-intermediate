aca1688c9b5a244317185454bd9501606678e77d
Zack Hartmann <zackhartmann@gmail.com>
2017-11-29 16:45:15 -0500

    $>find . -type d -name .idea -prune -exec git rm -r --cached "{}" \;

---
+++ Section 02/Lecture 09/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Se2Le9
+{
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            if (newLocation == null)
+                throw new ArgumentNullException("new location");
+            
+            Move(newLocation.X, newLocation.Y);
+        }
+    }
+}
---
+++ Section 02/Lecture 09/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Se2Le9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+        }
+
+        public static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        public static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                Console.WriteLine("Point is at ({0}, {1}", point.X, point.Y);
+            }
+            catch
+            {
+                Console.WriteLine("an unexpected error occcurred");
+                throw;
+            }  
+        }
+    }
+}
