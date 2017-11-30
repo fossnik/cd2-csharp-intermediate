db7e77dc86e8f3065f40e6d018f469096e59b3ab
Zack Hartmann <zackhartmann@gmail.com>
2017-11-30 15:58:51 -0500

    Lecture 11

---
+++ Section 02/Lecture 12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture12
+{
+    public class Person
+    {
+        public DateTime Birthdate { get; set; }
+        
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
