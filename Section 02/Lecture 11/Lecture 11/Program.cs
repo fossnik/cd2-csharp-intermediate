6778c28455c6a4b27fd5e2ef54732a209446048b
Zack Hartmann <zackhartmann@gmail.com>
2017-11-30 15:39:51 -0500

    Lecture 11

---
+++ Section 02/Lecture 11/Lecture 11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture11
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
