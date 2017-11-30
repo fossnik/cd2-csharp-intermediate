ac2576f562ffc1b227e111082eb1300b923c1835
Zack Hartmann <zackhartmann@gmail.com>
2017-11-30 17:57:36 -0500

    Refactored Directory Structure

---
+++ Section 02/Lecture 13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Section 02/Section 02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
