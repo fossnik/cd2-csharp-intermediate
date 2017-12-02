f4fb4d027ef593cdf91eeb9a4f96f26dc249e460
Zack Hartmann <zackhartmann@gmail.com>
2017-12-02 13:15:33 -0500

    Lecture 17

---
+++ Section 03/Lecture 17/PresentationObject.cs
@@ -0,0 +1,18 @@
+﻿namespace Lecture17
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            
+        }
+
+        public void Duplicate()
+        {
+            
+        }
+    }
+}
---
+++ Section 03/Lecture 17/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Lecture17
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+        }
+    }
+}
