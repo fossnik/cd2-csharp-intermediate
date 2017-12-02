199c6fab68bfea229f50b7b4fa0001a59cfeb85e
Zack Hartmann <zackhartmann@gmail.com>
2017-12-02 11:33:02 -0500

    Implemented Stopwatch

---
+++ Section 02/Exercise 01 - Stopwatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+
+namespace Exercise01_Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+
+        public void Start()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        public double Stop()
+        {
+            return (DateTime.Now - _startTime).TotalSeconds;
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch _stopwatch = new Stopwatch();
+            
+            while (true)
+            {
+                Console.WriteLine("Press any key to start");
+                Console.ReadLine();
+                
+                _stopwatch.Start();
+                
+                Console.WriteLine("Press any key to stop");
+                Console.ReadLine();
+                
+                Console.WriteLine("Elapsed time is {0} seconds\n\n", _stopwatch.Stop());
+            }
+        }
+    }
+}
---
+++ Section 02/Exercise 02 - StackOverflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise02_StackOverflow
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
