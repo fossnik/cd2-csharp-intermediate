5e46581245e5e518d53c8fc0f2db7f6e5748493a
Zack Hartmann <zackhartmann@gmail.com>
2017-12-04 20:40:58 -0500

    Section 04 - Initial

---
+++ Section 04/Lecture 21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture21
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
