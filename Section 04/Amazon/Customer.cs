33ea6ce2ab0de532d827457fdf8882df2c513998
Zack Hartmann <zackhartmann@gmail.com>
2017-12-05 19:27:22 -0500

    Implemented Class Libary

---
+++ Section 04/Amazon/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lecture21
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            Console.WriteLine("Promote logic changed.");
+        }
+
+        protected int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
--- Section 04/Lecture 21/Program.cs
+++ Section 04/Lecture 21/Program.cs
@@ -3,6 +3,14 @@ using System.Runtime.Remoting.Messaging;
 
 namespace Lecture21
 {
+    public class GoldCustomer : Customer
+    {
+        public void OfferVouchar()
+        {
+            var rating = this.CalculateRating(excludeOrders: true);
+            
+        }
+    }
     internal class Program
     {
         public static void Main(string[] args)
