72f0e8b22c0bc5218538cfa25c0dacd49cd10f26
Zack Hartmann <zackhartmann@gmail.com>
2017-12-06 14:43:34 -0500

    Lecture 23 - Casting

---
+++ Section 04/Lecture 23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture23
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Text text = new Text();
+            Shape shape = new Shape();
+        }
+    }
+}
