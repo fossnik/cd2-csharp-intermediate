6760b714fb9d1e42d4fe8db244bcdf5a9d935457
Zack Hartmann <zackhartmann@gmail.com>
2017-12-07 14:59:52 -0500

    Exercises - Inheritance

---
+++ Section 04/Exercises - Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises_Inheritance
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
