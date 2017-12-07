a09a0e9ca9d4baa58f4c330b0b9e963b1a60705a
Zack Hartmann <zackhartmann@gmail.com>
2017-12-06 20:34:55 -0500

    Lecture 24 - Boxing and Unboxing

---
+++ Section 04/Lecture 24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lecture24
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Math");
+            list.Add(DateTime.Today);
+            
+            var anotherList = new List<int>();
+        }
+    }
+}
