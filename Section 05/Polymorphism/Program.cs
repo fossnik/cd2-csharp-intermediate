28cdf94e941fc192b0551871443ea48c5849e377
Zack Hartmann <zackhartmann@gmail.com>
2017-12-12 18:28:39 -0500

    New Polymorph

---
+++ Section 05/Polymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
